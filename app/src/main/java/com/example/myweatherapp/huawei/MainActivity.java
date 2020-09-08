e890a429b1fc1f5e460750ff303c029ee7431457
Yan LIU <yann.liu@outlook.fr>
2020-09-08 18:23:49 +0200

    Weather App with HMS

---
+++ app/src/main/java/com/example/myweatherapp/huawei/MainActivity.java
@@ -0,0 +1,384 @@
+package com.example.myweatherapp.huawei;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import android.Manifest;
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.huawei.hmf.tasks.OnFailureListener;
+import com.huawei.hmf.tasks.OnSuccessListener;
+import com.huawei.hms.location.FusedLocationProviderClient;
+import com.huawei.hms.location.HWLocation;
+import com.huawei.hms.location.LocationCallback;
+import com.huawei.hms.location.LocationRequest;
+import com.huawei.hms.location.LocationServices;
+import com.huawei.hms.location.SettingsClient;
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.IOException;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView view_city;
+    TextView view_temp;
+    TextView view_desc;
+
+
+
+
+    ImageView view_weather;
+    EditText search;
+    FloatingActionButton search_floating;
+    FloatingActionButton location_floating;
+
+    public static final String TAG = "LocationUpdatesCallback";
+    // the callback of the request
+    LocationCallback mLocationCallback;
+
+    LocationRequest mLocationRequest;
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
+
+    private SettingsClient settingsClient;
+
+    private HWLocation userLocation;
+
+    private String city;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        MyReceiver receiver = new MyReceiver();
+        IntentFilter filter=new IntentFilter();
+        filter.addAction("com.huawei.codelabpush.ON_NEW_TOKEN");
+        MainActivity.this.registerReceiver(receiver,filter);
+
+
+
+        setContentView(R.layout.activity_main);
+        // create fusedLocationProviderClient
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        // create settingsClient
+        settingsClient = LocationServices.getSettingsClient(this);
+        mLocationRequest = new LocationRequest();
+        // Set the interval for location updates, in milliseconds.
+        mLocationRequest.setInterval(10000);
+        /**
+         * issue: can not get device country
+         */
+        // Get Address
+        mLocationRequest.setNeedAddress(true);
+        // set the priority of the request
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        view_city=findViewById(R.id.town);
+        view_city.setText("");
+        view_temp=findViewById(R.id.temp);
+        view_temp.setText("");
+        view_desc=findViewById(R.id.desc);
+        view_desc.setText("");
+
+        view_weather=findViewById(R.id.wheather_image);
+        search=findViewById(R.id.search_edit);
+        search_floating=findViewById(R.id.floating_search);
+        location_floating=findViewById(R.id.floating_location);
+
+        // check location permisiion
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+            Log.i(TAG, "sdk < 28 Q");
+            if (ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                String[] strings =
+                        {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+                ActivityCompat.requestPermissions(this, strings, 1);
+            }
+        } else {
+            if (ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this,
+                    "android.permission.ACCESS_BACKGROUND_LOCATION") != PackageManager.PERMISSION_GRANTED) {
+                String[] strings = {android.Manifest.permission.ACCESS_FINE_LOCATION,
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                        "android.permission.ACCESS_BACKGROUND_LOCATION"};
+                ActivityCompat.requestPermissions(this, strings, 2);
+            }
+        }
+
+        search_floating.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //hide Keyboard
+                InputMethodManager imm=(InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(getCurrentFocus().getRootView().getWindowToken(),0);
+                api_key(String.valueOf(search.getText()));
+                Log.i("My activity", String.valueOf(search.getText()));
+            }
+        });
+
+        location_floating.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getLocation();
+                if (userLocation!=null)
+                {
+                    api_key_location(userLocation);
+                }
+
+            }
+        });
+
+
+
+
+    }
+
+
+
+    private void getLocation() {
+        try {
+            fusedLocationProviderClient
+                    .getLastLocationWithAddress(mLocationRequest)
+                    .addOnSuccessListener(new OnSuccessListener<HWLocation>() {
+
+                        @Override
+                        public void onSuccess(HWLocation hwLocation) {
+                            System.out.println("CITY >>> " + hwLocation.getCity() + "Latitude: "+ hwLocation.getLatitude() + " Longitude: "+ hwLocation.getLongitude());
+                            userLocation=hwLocation;
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(Exception e) {
+                            Log.e(TAG,
+                                    "requestLocationUpdatesWithCallback onFailure:" + e.getMessage());
+                        }
+                    });
+        } catch (Exception e) {
+            Log.e(TAG, "requestLocationUpdatesWithCallback exception:" + e.getMessage());
+        }
+    }
+    @Override
+    protected void onDestroy() {
+        // don't need to receive callback
+        removeLocationUpdatesWithCallback();
+        super.onDestroy();
+    }
+
+    private void removeLocationUpdatesWithCallback() {
+        try {
+            fusedLocationProviderClient.removeLocationUpdates(mLocationCallback)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Log.i(TAG, "removeLocationUpdatesWithCallback onSuccess");
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(Exception e) {
+                            Log.e(TAG, "removeLocationUpdatesWithCallback onFailure:" + e.getMessage());
+                        }
+                    });
+        } catch (Exception e) {
+            Log.e(TAG, "removeLocationUpdatesWithCallback exception:" + e.getMessage());
+        }
+    }
+
+    private void api_key_location(final HWLocation location) {
+        OkHttpClient client=new OkHttpClient();
+
+        Request request=new Request.Builder()
+                .url("https://api.openweathermap.org/data/2.5/weather?lat="+ String.valueOf(location.getLatitude())+"&lon="+String.valueOf(location.getLongitude())+"&appid=5eaf2c37a33b120efc73c69945dd79ae&units=metric")
+                .get()
+                .build();
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        try {
+            Response response= client.newCall(request).execute();
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(@NotNull Call call, @NotNull IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                    String responseData= response.body().string();
+                    try {
+                        JSONObject json=new JSONObject(responseData);
+                        JSONArray array=json.getJSONArray("weather");
+                        JSONObject object=array.getJSONObject(0);
+
+                        String description=object.getString("description");
+                        String icons = object.getString("icon");
+
+                        JSONObject temp1= json.getJSONObject("main");
+                        Double Temperature=temp1.getDouble("temp");
+
+                        city =location.getCity()==null? "unknown":location.getCity();
+                        setText(view_city, city+", "+location.getCountryName());
+                        Log.i(TAG,"---------------city: "+city);
+
+                        String temps=Math.round(Temperature)+" °C";
+                        setText(view_temp,temps);
+                        setText(view_desc,description);
+                        setImage(view_weather,icons);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void api_key(final String City) {
+        OkHttpClient client=new OkHttpClient();
+
+        Request request=new Request.Builder()
+                .url("https://api.openweathermap.org/data/2.5/weather?q="+City+"&appid=5eaf2c37a33b120efc73c69945dd79ae&units=metric")
+                .get()
+                .build();
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        try {
+            Response response= client.newCall(request).execute();
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(@NotNull Call call, @NotNull IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                    String responseData= response.body().string();
+                    try {
+                        JSONObject json=new JSONObject(responseData);
+                        JSONArray array=json.getJSONArray("weather");
+                        JSONObject object=array.getJSONObject(0);
+
+                        String description=object.getString("description");
+                        String icons = object.getString("icon");
+
+                        JSONObject temp1= json.getJSONObject("main");
+                        Double Temperature=temp1.getDouble("temp");
+
+                        setText(view_city,City);
+
+                        String temps=Math.round(Temperature)+" °C";
+                        setText(view_temp,temps);
+                        setText(view_desc,description);
+                        setImage(view_weather,icons);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+    }
+    private void setText(final TextView text, final String value){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                text.setText(value);
+            }
+        });
+    }
+    private void setImage(final ImageView imageView, final String value){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                //paste switch
+                switch (value){
+                    case "01d": imageView.setImageDrawable(getResources().getDrawable(R.drawable.d01d));
+                        break;
+                    case "01n": imageView.setImageDrawable(getResources().getDrawable(R.drawable.d01d));
+                        break;
+                    case "02d": imageView.setImageDrawable(getResources().getDrawable(R.drawable.d02d));
+                        break;
+                    case "02n": imageView.setImageDrawable(getResources().getDrawable(R.drawable.d02d));
+                        break;
+                    case "03d": imageView.setImageDrawable(getResources().getDrawable(R.drawable.d03d));
+                        break;
+                    case "03n": imageView.setImageDrawable(getResources().getDrawable(R.drawable.d03d));
+                        break;
+                    case "04d": imageView.setImageDrawable(getResources().getDrawable(R.drawable.d04d));
+                        break;
+                    case "04n": imageView.setImageDrawable(getResources().getDrawable(R.drawable.d04d));
+                        break;
+                    case "09d": imageView.setImageDrawable(getResources().getDrawable(R.drawable.d09d));
+                        break;
+                    case "09n": imageView.setImageDrawable(getResources().getDrawable(R.drawable.d09d));
+                        break;
+                    case "10d": imageView.setImageDrawable(getResources().getDrawable(R.drawable.d10d));
+                        break;
+                    case "10n": imageView.setImageDrawable(getResources().getDrawable(R.drawable.d10d));
+                        break;
+                    case "11d": imageView.setImageDrawable(getResources().getDrawable(R.drawable.d11d));
+                        break;
+                    case "11n": imageView.setImageDrawable(getResources().getDrawable(R.drawable.d11d));
+                        break;
+                    case "13d": imageView.setImageDrawable(getResources().getDrawable(R.drawable.d13d));
+                        break;
+                    case "13n": imageView.setImageDrawable(getResources().getDrawable(R.drawable.d13d));
+                        break;
+                    default:
+                        imageView.setImageDrawable(getResources().getDrawable(R.drawable.wheather));
+
+                }
+            }
+        });
+    }
+
+    public class MyReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            if ("com.huawei.codelabpush.ON_NEW_TOKEN".equals(intent.getAction())) {
+
+                String token = intent.getStringExtra("token");
+
+                Log.w("Push Token: ", token);
+
+            }
+
+        }
+
+    }
+}
